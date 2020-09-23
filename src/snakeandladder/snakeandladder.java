64e667ebd25adea52ae8dc3e6d37c9a0191934e7
utkarshmega <utkarsheduc@gmail.com>
2020-09-23 12:07:23 +0530

    First Commit

---
+++ src/snakeandladder/snakeandladder.java
@@ -0,0 +1,11 @@
+package snakeandladder;
+
+public class snakeandladder {
+	
+	public static void main(String[] args) {
+		
+		int pos = 0;
+		System.out.println("Single player at position "+pos);
+	}
+	
+}
