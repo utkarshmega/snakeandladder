3ba85a24e3d02c7094dea7427e02eaf67c8771f4
utkarshmega <utkarsheduc@gmail.com>
2020-09-23 12:27:41 +0530

    UC3 Completed

--- src/snakeandladder/snakeandladder.java
+++ src/snakeandladder/snakeandladder.java
@@ -7,9 +7,27 @@ public class snakeandladder {
 		
 		Random rnd = new Random();
 		
+		int ladder = 1;
+		int snake = 2;
+		int noplay = 0;
+		int pos = 0;
 		int dice = rnd.nextInt(6) + 1;
-		
 		System.out.println("Number on dice = "+dice);
+		int play = rnd.nextInt(3);
+		
+		if(play != noplay) {
+			if(play==ladder)
+				pos += dice;
+			
+			else if(play == snake)
+				pos -= dice;
+			
+			System.out.println("New position = "+pos);
+		}
+		else 
+			System.out.println("No play");
+		
+		
 	}
 	
 }
