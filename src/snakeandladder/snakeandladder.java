75ee58c99ade7ef5cb2e7a013cfc15b8412a41ef
utkarshmega <utkarsheduc@gmail.com>
2020-09-23 12:55:07 +0530

    UC6 Completed

--- src/snakeandladder/snakeandladder.java
+++ src/snakeandladder/snakeandladder.java
@@ -11,13 +11,13 @@ public class snakeandladder {
 		int snake = 2;
 		int noplay = 0;
 		int pos = 0;
-		
+		int nooftimes = 0;
 		//System.out.println("Number on dice = "+dice);
-		while(pos<=100)
+		while(pos!=100)
 		{
 			int dice = rnd.nextInt(6) + 1;
 			int play = rnd.nextInt(3);
-			
+			nooftimes++;
 			if(play != noplay) {
 				if(play==ladder)
 				{
@@ -36,6 +36,7 @@ public class snakeandladder {
 				System.out.println("New position = "+pos);
 			}
 		}
+		System.out.println("Number of times the dice was thrown is = "+nooftimes);
 		System.out.println("The player reached the exact position 100 and won");
 		
 	}
